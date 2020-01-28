62fbe117651bd792b1df6a2bcc6a9b65b1dba7c0
Simon <simonsch.tz@gmail.com>
2020-01-28 16:15:17 +0100

    One step closer to full functionality, bg header still has problems

--- script/switch.js
+++ script/switch.js
@@ -3,6 +3,7 @@
     const html = document.querySelector("html")
     const body = document.querySelector("body");
     const nav = document.querySelector("nav");
+    const header = document.querySelector("header")
 
     //Color Gradient for header
     const gradientAP = "linear-gradient( rgb(203, 201, 203), rgb(203, 201, 203), rgb(203, 201, 203), rgb(203, 201, 203), rgb(203, 201, 203), rgba(255, 255, 0, 0))"
@@ -13,16 +14,18 @@
         let headerBG = document.querySelector("header");
 
         if (pageSwitch.checked) {
-            headerBG.style.backgroundImage = gradientSE;
+            // headerBG.style.backgroundImage = gradientSE;
             html.classList.add("SE")
             body.classList.add("SE")
             nav.classList.add("SE")
+            header.classList.add("SE")
 
         } else {
-            headerBG.style.backgroundImage = gradientAP;
+            // headerBG.style.backgroundImage = gradientAP;
             html.classList.remove("SE")
             body.classList.remove("SE")
             nav.classList.remove("SE")
+            header.classList.remove("SE")
 
         }
         // if (!pageSwitch.checked) {
