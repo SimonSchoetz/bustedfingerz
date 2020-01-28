220b7f3ceea525f786ce97482e1bdd8fc5cf4601
Simon <simonsch.tz@gmail.com>
2020-01-28 15:45:56 +0100

    Make colors switch to SE page when switch is switched

--- script/switch.js
+++ script/switch.js
@@ -1,14 +1,29 @@
 (() => {
     const pageSwitch = document.querySelector("#switch");
+    const html = document.querySelector("html")
+    const body = document.querySelector("body");
+    const nav = document.querySelector("nav");
+
+    //Color Gradient for header
+    const gradientAP = "linear-gradient( rgb(203, 201, 203), rgb(203, 201, 203), rgb(203, 201, 203), rgb(203, 201, 203), rgb(203, 201, 203), rgba(255, 255, 0, 0))"
+    const gradientSE = "linear-gradient( rgb(41, 40, 41), rgb(41, 40, 41), rgb(41, 40, 41), rgb(41, 40, 41), rgb(41, 40, 41), rgba(255, 255, 0, 0))"
+
 
     const switchClasses = () => {
-
-        const body = document.querySelector("body");
+        let headerBG = document.querySelector("header");
 
         if (pageSwitch.checked) {
+            headerBG.style.backgroundImage = gradientSE;
+            html.classList.add("SE")
             body.classList.add("SE")
+            nav.classList.add("SE")
+
         } else {
+            headerBG.style.backgroundImage = gradientAP;
+            html.classList.remove("SE")
             body.classList.remove("SE")
+            nav.classList.remove("SE")
+
         }
         // if (!pageSwitch.checked) {
         //     console.log("unchecked!")
