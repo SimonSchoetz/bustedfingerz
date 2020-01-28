957344b0487f377dc2a3c6be7cc7492c55c46c53
Simon <simonsch.tz@gmail.com>
2020-01-28 09:53:13 +0100

    Add pseudo code to switch.js

--- switch.js
+++ switch.js
@@ -1,1 +1,12 @@
-git
+const pageSwitch = switchState => {
+    // get switch state
+    // unchecked = 
+    // -> base / delete yet to make style class
+    // -> hide mastering page
+    // -> show artist page nav & content
+    // checked = 
+    // -> add yet to make style class
+    // -> hide artist page
+    // -> show mastering nav & page
+
+}
