a20cdd1380f7b89b509df1ec5f50efc6fbddb687
Simon <simonsch.tz@gmail.com>
2020-01-29 09:52:23 +0100

    Refactore switch.js

--- script/switch.js
+++ script/switch.js
@@ -1,47 +1,20 @@
 (() => {
+    //Array with selectors
+    const selectors = ["html", "body", "header", "nav"]
+
+    // for addEventListener and if statement
     const pageSwitch = document.querySelector("#switch");
-    const html = document.querySelector("html")
-    const body = document.querySelector("body");
-    const nav = document.querySelector("nav");
-    const header = document.querySelector("header")
-
-    //Color Gradient for header
-    const gradientAP = "linear-gradient( rgb(203, 201, 203), rgb(203, 201, 203), rgb(203, 201, 203), rgb(203, 201, 203), rgb(203, 201, 203), rgba(255, 255, 0, 0))"
-    const gradientSE = "linear-gradient( rgb(41, 40, 41), rgb(41, 40, 41), rgb(41, 40, 41), rgb(41, 40, 41), rgb(41, 40, 41), rgba(255, 255, 0, 0))"
-
 
     const switchClasses = () => {
-        let headerBG = document.querySelector("header");
+        selectors.forEach(el => {
+            const selector = document.querySelector(el)
+            if (pageSwitch.checked) {
+                selector.classList.add("SE")
 
-        if (pageSwitch.checked) {
-            // headerBG.style.backgroundImage = gradientSE;
-            html.classList.add("SE")
-            body.classList.add("SE")
-            nav.classList.add("SE")
-            header.classList.add("SE")
-
-        } else {
-            // headerBG.style.backgroundImage = gradientAP;
-            html.classList.remove("SE")
-            body.classList.remove("SE")
-            nav.classList.remove("SE")
-            header.classList.remove("SE")
-
-        }
-        // if (!pageSwitch.checked) {
-        //     console.log("unchecked!")
-        // }
-
-        // unchecked = 
-        // -> base / delete yet to make style class
-        // -> hide mastering page
-        // -> show artist page nav & content
-        // checked = 
-        // -> add yet to make style class
-        // -> hide artist page
-        // -> show mastering nav & page
-
+            } else {
+                selector.classList.remove("SE")
+            }
+        })
     }
     pageSwitch.addEventListener("click", switchClasses);
-
 })();
